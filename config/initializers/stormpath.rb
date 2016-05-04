3efef9d230c51b4144b43a75bb068897eea1eb13
Michael Lobel <mglobel@gmail.com>
2016-05-04 18:31:09 -0500

    match stormpath env vars to heroku default

--- config/initializers/stormpath.rb
+++ config/initializers/stormpath.rb
@@ -1,5 +1,5 @@
 Stormpath::Rails.configure do |config|
-  config.api_key.id = ENV['STORMPATH_CLIENT_APIKEY_ID']
-  config.api_key.secret = ENV['STORMPATH_CLIENT_APIKEY_SECRET']
-  config.application.href = ENV['STORMPATH_APPLICATION_HREF']
+  config.api_key.id = ENV['STORMPATH_API_KEY_ID']
+  config.api_key.secret = ENV['STORMPATH_API_KEY_SECRET']
+  config.application.href = ENV['STORMPATH_URL']
 end
